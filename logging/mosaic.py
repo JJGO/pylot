225b0adfab8ee8c99ab23aed4585d913784b0b9e
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-08-14 10:52:06 -0400

    Refactor Mosaic logger for self supervised tasks

--- logging/mosaic.py
+++ logging/mosaic.py
@@ -1,16 +1,20 @@
 import numpy as np
 import torch
+from PIL import Image
 
 class TrainValidMosaic:
 
-    def __init__(self, model, x_train, y_train, x_valid, y_valid, x_fn=None):
+    def __init__(self, model, x_train, x_valid, y_train=None, y_valid=None, x_fn=None):
         self.model = model
 
         self.x_train = x_train
         self.x_valid = x_valid
 
         B, C_in, H, W  = x_train.shape
-        _, C_out, _, _  = y_train.shape
+        if y_train is not None:
+            _, C_out, _, _  = y_train.shape
+        else:
+            C_out = C_in
 
         self.H_mosaic = B*H
         self.W_mosaic = W*6
@@ -19,14 +23,25 @@ class TrainValidMosaic:
         if x_fn is None:
             x_fn = lambda x: x
 
-        self.columns = [
+        self.x_columns = [
             x_fn(self._to_numpy_img(x_train)),
-            None,
-            self._to_numpy_img(y_train),
             x_fn(self._to_numpy_img(x_valid)),
-            None,
-            self._to_numpy_img(y_valid),
         ]
+
+        if y_train is not None and y_valid is not None:
+
+            self.y_columns = [
+                self._to_numpy_img(y_train),
+                self._to_numpy_img(y_valid),
+            ]
+
+        else:
+            self.y_columns = [
+                np.zeros((self.H_mosaic, 0, self.C_mosaic)),
+                np.zeros((self.H_mosaic, 0, self.C_mosaic))
+            ]
+
+
 
     def _to_numpy_img(self, x):
         x = x.detach().cpu().numpy()
@@ -35,11 +50,26 @@ class TrainValidMosaic:
         x = x.transpose((0,2,3,1))
         return x.reshape(self.H_mosaic, -1, self.C_mosaic)
 
-    def capture(self):
+    def capture(self, output_idx=None):
         with torch.no_grad():
-            self.columns[1] = self._to_numpy_img(self.model(self.x_train))
-            self.columns[4] = self._to_numpy_img(self.model(self.x_valid))
+            if output_idx is None:
+                yh_columns = [
+                    self._to_numpy_img(self.model(self.x_train)),
+                    self._to_numpy_img(self.model(self.x_valid))
+                ]
+            else:
+                yh_columns = [
+                    self._to_numpy_img(self.model(self.x_train)[output_idx]),
+                    self._to_numpy_img(self.model(self.x_valid)[output_idx])
+                ]
 
-            img = np.hstack(self.columns)
+            columns = list(zip(self.x_columns, yh_columns, self.y_columns))
+            columns = columns[0] + columns[1]
+
+            img = np.hstack(columns)
+
+        # img = (img*255).astype(np.uint8)
+        # if img.shape[-1] == 1:
+        #     img = img[...,0]
 
         return img
