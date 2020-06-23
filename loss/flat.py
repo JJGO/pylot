37545aea5249b7ad6f11482991e8dcb074295c74
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-06-23 18:14:54 -0400

    loss flattener decorator

---
+++ loss/flat.py
@@ -0,0 +1,9 @@
+import torch
+
+def flatten_loss(loss_func):
+
+    def flattened_loss(yhat, y):
+        batch_size = y.size()[0]
+        return loss_func(yhat.view(batch_size, -1), y.view(batch_size, -1))
+
+    return flattened_loss
