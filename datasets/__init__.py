70924d7e406eae858f99e95e70172451b9205b78
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-06-26 19:42:50 -0400

    Rename datasets as vision

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,10 +3,10 @@
 The wrappers here include proper
 """
 
-from .datasets import (MNIST,
-                       CIFAR10,
-                       CIFAR100,
-                       ImageNet,
-                       Places365,
-                       TinyImageNet,
-                       Miniplaces)
+from .vision import (MNIST,
+                     CIFAR10,
+                     CIFAR100,
+                     ImageNet,
+                     Places365,
+                     TinyImageNet,
+                     Miniplaces)
