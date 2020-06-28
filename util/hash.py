dd257758a5cad91fb21b1d4a39aee0446b72154e
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-06-28 16:28:54 -0400

    Add universal hash function

---
+++ util/hash.py
@@ -0,0 +1,24 @@
+import copy
+
+
+def make_hash(o):
+
+    """
+    Makes a hash from a dictionary, list, tuple or set to any level, that contains
+    only other hashable types (including any lists, tuples, sets, and
+    dictionaries).
+    """
+
+    if isinstance(o, (set, tuple, list)):
+
+        return tuple([make_hash(e) for e in o])
+
+    elif not isinstance(o, dict):
+
+        return hash(o)
+
+    new_o = copy.deepcopy(o)
+    for k, v in new_o.items():
+        new_o[k] = make_hash(v)
+
+    return hash(tuple(frozenset(sorted(new_o.items()))))
