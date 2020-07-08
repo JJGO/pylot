1ce94ee214e37852977d411918c8d291d4cd5176
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-07-08 11:24:38 -0400

    Add contexmanager to modify dfs inplace

--- plot/data.py
+++ plot/data.py
@@ -1,5 +1,7 @@
 from collections import Counter
+from contextlib import contextmanager
 import pandas as pd
+
 
 def df_filter(df, **kwargs):
 
@@ -11,6 +13,7 @@ def df_filter(df, **kwargs):
         #     selector |= (df == v)
         # df = df[selector]
     return df
+
 
 def augment_df(df, fns):
     for fn in fns:
@@ -24,9 +27,17 @@ def unique_combinations(df, cols):
         count[tuple(x)] += 1
     data = []
     for c, v in count.items():
-        data.append(list(c)+[v])
-    uniq = pd.DataFrame(data, columns=cols+['count']).sort_values(by=cols)
+        data.append(list(c) + [v])
+    uniq = pd.DataFrame(data, columns=cols + ["count"]).sort_values(by=cols)
     return uniq
+
 
 def unique_per_col(df):
     return df.apply(lambda x: set([str(i) for i in x]))
+
+
+@contextmanager
+def inplace_pandas_csv(path):
+    df = pd.read_csv(path)
+    yield df
+    df.to_csv(path, index=False)
