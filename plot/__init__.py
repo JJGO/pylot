d4d9e0680025e2ab026f2b1218ac15546db11e9a
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-08-02 22:16:15 -0400

    Add factor plot function

--- plot/__init__.py
+++ plot/__init__.py
@@ -6,5 +6,9 @@ from ..util import AutoMap
 from .cmap import Monokai
 from .turbo import Turbo
 
+from .ui import PlotUI
+
+from .factor import factor_plot, factor_plot_df
+
 # CMAP = plt.get_cmap('Set1')
 # colors = AutoMap(CMAP.colors)
---
+++ plot/factor.py
@@ -0,0 +1,68 @@
+import functools
+import warnings
+import numpy as np
+from scipy import stats
+
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LogNorm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pylot.analysis import filter_df
+
+viridis_high = ListedColormap(cm.get_cmap("viridis").colors[64:])
+
+
+def infer_norm(array):
+    if array.dtype == "O":
+        return None
+    mean = np.mean(array)
+    gmean = stats.gmean(array[np.nonzero(array)[0]])
+    median = np.median(array)
+
+    if np.abs(mean - median) > np.abs(gmean - median):
+        return LogNorm()
+    return None
+
+
+def factor_plot(data, x, y, hue, col, row, style, xlog=False, ylog=False, **kwargs):
+    data = filter_df(data, **kwargs)
+    if len(data) == 0:
+        warnings.warn("Empty dataset")
+        return
+    height = 16
+    if row is not None:
+        height /= data[row].nunique()
+    height = min(height, 8)
+    width = 10
+    aspect = width / height
+    fig = sns.relplot(
+        x=x,
+        y=y,
+        hue=hue,
+        data=data,
+        kind="line",
+        legend="full",
+        palette=viridis_high,
+        col=col,
+        row=row,
+        style=style,
+        hue_norm=infer_norm(data[hue].unique()),
+        markevery=1,
+        markers=True,
+        height=height,
+        aspect=aspect,
+        markersize=5,
+        err_style="band",
+    )
+    if xlog:
+        plt.xscale("log")
+
+    if ylog:
+        plt.yscale("log")
+
+    return fig
+
+
+def factor_plot_df(data):
+    return functools.partial(factor_plot, data)
