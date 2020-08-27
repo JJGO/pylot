1a1e4c3bdec6bdb663f81a3315736aff9a1a4184
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-08-27 12:09:16 -0400

    Temporary fix on model checkpoint for experiment compatibility

--- callbacks/checkpoint.py
+++ callbacks/checkpoint.py
@@ -2,9 +2,17 @@ def Checkpoint(experiment, save_freq=1):
     i = 0
 
     def CheckpointCallback(epoch):
+        # TODO think about a cleaner way
+        # Maybe define callbacks for both epoch start and end
+        # instead of just at end?
         nonlocal i
         i += 1
         if i % save_freq == 0:
-            experiment.checkpoint(tag=f"{epoch:03d}")
+            # For resumable experiments is easier to think about
+            # Checkpoint to be at the beginning of epoch rather
+            # than at the end
+            experiment._epoch += 1
+            experiment.checkpoint(tag=f"{experiment._epoch:03d}")
+            experiment._epoch -= 1
 
     return CheckpointCallback
