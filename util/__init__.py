0d841f34a52f14fc5c3e153c5ab1cc5a5f09e496
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-08-30 01:03:14 -0400

    Fmt compliance

--- util/__init__.py
+++ util/__init__.py
@@ -3,7 +3,13 @@ from .color import printc, highlight, colors, setup_colored_traceback
 from .csvlogger import CSVLogger, SimpleCSVLogger
 from .filecache import FileCache
 from .fzf import fzf
-from .mapping import expand_dots, expand_keys, delete_with_prefix, dict_recursive_update, allbut
+from .mapping import (
+    expand_dots,
+    expand_keys,
+    delete_with_prefix,
+    dict_recursive_update,
+    allbut,
+)
 from .meta import separate_kwargs
 from .meter import StatsMeter, MaxMinMeter, MeterCSVLogger, Meter, UnionMeter
 from .print import printy
