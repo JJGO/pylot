3d55ee92500123513f889b3006aa0308d543d6d7
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-09-03 17:55:40 -0400

    Allow summary to return values as a dict

--- log/summary.py
+++ log/summary.py
@@ -7,12 +7,11 @@ import torch.nn as nn
 from torchviz import make_dot
 
 
-def summary(model, input_size, batch_size=-1, device="cuda", echo=True):
+def summary(model, input_size, batch_size=-1, device="cuda", echo=True, as_stats=False):
 
     out = ""
 
     def register_hook(module):
-
         def hook(module, input, output):
             class_name = str(module.__class__).split(".")[-1].split("'")[0]
             module_idx = len(summary)
@@ -76,10 +75,10 @@ def summary(model, input_size, batch_size=-1, device="cuda", echo=True):
     for h in hooks:
         h.remove()
 
-    out += ("----------------------------------------------------------------") + '\n'
+    out += ("----------------------------------------------------------------") + "\n"
     line_new = "{:>20}  {:>25} {:>15}".format("Layer (type)", "Output Shape", "Param #")
-    out += (line_new) + '\n'
-    out += ("================================================================") + '\n'
+    out += (line_new) + "\n"
+    out += ("================================================================") + "\n"
     total_params = 0
     total_output = 0
     trainable_params = 0
@@ -95,25 +94,39 @@ def summary(model, input_size, batch_size=-1, device="cuda", echo=True):
         if "trainable" in summary[layer]:
             if summary[layer]["trainable"] == True:
                 trainable_params += summary[layer]["nb_params"]
-        out += (line_new) + '\n'
+        out += (line_new) + "\n"
 
     # assume 4 bytes/number (float on cuda).
-    total_input_size = abs(np.prod(input_size) * batch_size * 4. / (1024 ** 2.))
-    total_output_size = abs(2. * total_output * 4. / (1024 ** 2.))  # x2 for gradients
-    total_params_size = abs(total_params * 4. / (1024 ** 2.))
+    total_input_size = abs(np.prod(input_size) * batch_size * 4.0 / (1024 ** 2.0))
+    total_output_size = abs(
+        2.0 * total_output * 4.0 / (1024 ** 2.0)
+    )  # x2 for gradients
+    total_params_size = abs(total_params * 4.0 / (1024 ** 2.0))
     total_size = total_params_size + total_output_size + total_input_size
 
-    out += ("================================================================") + '\n'
-    out += ("Total params: {0:,}".format(total_params)) + '\n'
-    out += ("Trainable params: {0:,}".format(trainable_params)) + '\n'
-    out += ("Non-trainable params: {0:,}".format(total_params - trainable_params)) + '\n'
-    out += ("----------------------------------------------------------------") + '\n'
-    out += ("Input size (MB): %0.2f" % total_input_size) + '\n'
-    out += ("Forward/backward pass size (MB): %0.2f" % total_output_size) + '\n'
-    out += ("Params size (MB): %0.2f" % total_params_size) + '\n'
-    out += ("Estimated Total Size (MB): %0.2f" % total_size) + '\n'
-    out += ("----------------------------------------------------------------") + '\n'
+    out += ("================================================================") + "\n"
+    out += ("Total params: {0:,}".format(total_params)) + "\n"
+    out += ("Trainable params: {0:,}".format(trainable_params)) + "\n"
+    out += (
+        "Non-trainable params: {0:,}".format(total_params - trainable_params)
+    ) + "\n"
+    out += ("----------------------------------------------------------------") + "\n"
+    out += ("Input size (MB): %0.2f" % total_input_size) + "\n"
+    out += ("Forward/backward pass size (MB): %0.2f" % total_output_size) + "\n"
+    out += ("Params size (MB): %0.2f" % total_params_size) + "\n"
+    out += ("Estimated Total Size (MB): %0.2f" % total_size) + "\n"
+    out += ("----------------------------------------------------------------") + "\n"
     # return summary
+
+    if as_stats:
+        return {
+            "total_params": int(total_params.item()),
+            "trainable_params": int(trainable_params.item()),
+            "input_size": int(total_input_size.item()) * 1024 ** 2,
+            "backprop_size": int(total_output_size.item()) * 1024 ** 2,
+            "params_size": int(total_params_size.item()) * 1024 ** 2,
+            "total_size": int(total_size.item()) * 1024 ** 2,
+        }
 
     if echo:
         print(out)
@@ -123,16 +136,16 @@ def summary(model, input_size, batch_size=-1, device="cuda", echo=True):
 
 def record_model(model, x, path=None):
     if path is None:
-        path = '.'
+        path = "."
     path = pathlib.Path(path)
 
     # Generate topology
     yhat = model(x)
     g = make_dot(yhat)
-#     g.format = 'svg'
-    g.render(path / 'topology')
+    #     g.format = 'svg'
+    g.render(path / "topology")
 
     # Print summary
-    with open(path / 'summary.txt', 'w') as f:
+    with open(path / "summary.txt", "w") as f:
         s = summary(model, x.shape[1:], echo=False)
         print(s, file=f)
