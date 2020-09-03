9e1ed7e22a066920f56c2bff87a79ce5c7950095
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-09-03 17:55:06 -0400

    Implement stratified split for datasets w/o explicit val

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,7 @@
 The wrappers here include proper
 """
 
+from .util import train_val_split, stratified_train_val_split
 from .vision import (MNIST,
                      CIFAR10,
                      CIFAR100,
---
+++ datasets/util.py
@@ -0,0 +1,23 @@
+import math
+import numpy as np
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import random_split, Subset
+
+
+def train_val_split(dataset, val_split, seed=None):
+    dataset_size = len(dataset)
+    val_size = math.ceil(val_split * dataset_size)
+    train_size = dataset_size - val_size
+    kwargs = {}
+    if seed is not None:
+        kwargs["generator"] = torch.Generator().manual_seed(seed)
+    return random_split(dataset, [train_size, val_size], **kwargs)
+
+
+def stratified_train_val_split(dataset, val_split, seed=None):
+    indices = np.arange(len(dataset))
+    train_indices, val_indices = train_test_split(
+        indices, test_size=val_split, random_state=seed, stratify=dataset.targets
+    )
+    return Subset(dataset, train_indices), Subset(dataset, val_indices)
