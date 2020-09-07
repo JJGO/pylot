6bc9d9d8f076021c9892581a997f08ef181e3c03
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-09-07 00:19:44 -0400

    Better errors on dynamic module fetching

--- experiment/util.py
+++ experiment/util.py
@@ -25,4 +25,5 @@ def any_getattr(modules, attr):
     for module in reversed(modules):
         if hasattr(module, attr):
             return getattr(module, attr)
-    raise ImportError(f"Attribute {attr} not found in any of {modules}")
+    module_names = [m.__name__ for m in modules]
+    raise ImportError(f"Attribute {attr} not found in any of {module_names}")
