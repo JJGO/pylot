fab70e87d5c4cfb70fa7b0a7ca987a2354ac4d65
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-03-02 10:31:37 -0500

    Selfsupervised dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,6 +6,7 @@ The wrappers here include proper
 from .util import train_val_split, stratified_train_val_split
 from .cache import IndexedImageFolder, IndexedDatasetFolder, IndexedImageDataset
 from .subset import subset_dataset
+from .selfsuper import SelfsuperDataset, self_supervised
 from .vision import (MNIST,
                      CIFAR10,
                      CIFAR100,
---
+++ datasets/selfsuper.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from functools import wraps
+from typing import Tuple
+
+from torch import Tensor
+from torch.utils.data import Dataset
+
+
+@dataclass(eq=False)
+class SelfsuperDataset(Dataset):
+
+    inner_dataset: Dataset
+
+    def __getitem__(self, index) -> Tuple[Tensor, Tensor]:
+        x = self.inner_dataset[index]
+        if isinstance(x, tuple):
+            x = x[0]
+        return x, x
+
+    def __len__(self) -> int:
+        return len(self.inner_dataset)  # type: ignore
+
+
+def self_supervised(dataset: Dataset) -> Dataset:
+    def duplicate(f):
+        @wraps(f)
+        def duplicated(index):
+            x = f(index)
+            if isinstance(x, tuple):
+                x = x[0]
+            return x, x
+
+        return duplicated
+
+    dataset.__getitem__ = duplicate(dataset.__getitem__)  # type: ignore
+    return dataset
