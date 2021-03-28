237a13f1f5b50aee290701423c03fdee6260f5e8
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-03-27 20:21:25 -0400

    Fix batch_norm placement in conv block

--- conv/block.py
+++ conv/block.py
@@ -1,11 +1,15 @@
 from torch import nn
 
-def ConvBlock(inplanes,
-              filters,
-              dims=2,
-              activation='LeakyReLU',  #'ReLU',
-              batch_norm=True,
-              kernel_size=3):
+
+def ConvBlock(
+    inplanes,
+    filters,
+    dims=2,
+    activation="LeakyReLU",
+    batch_norm=True,
+    kernel_size=3,
+    residual=False,  # TODO add residual connections
+):
 
     if activation is not None:
         nonlinearity = getattr(nn, activation)
@@ -14,16 +18,19 @@ def ConvBlock(inplanes,
     bn_fn = getattr(nn, f"BatchNorm{dims}d")
 
     ops = []
-    for i, (n_in, n_out) in enumerate(zip([inplanes]+filters, filters)):
-        conv = conv_fn(n_in, n_out,
-                       kernel_size=kernel_size,
-                       padding=kernel_size//2,
-                       padding_mode='zeros')
+    for n_in, n_out in zip([inplanes] + filters, filters):
+        conv = conv_fn(
+            n_in,
+            n_out,
+            kernel_size=kernel_size,
+            padding=kernel_size // 2,
+            padding_mode="zeros",
+        )
         ops.append(conv)
 
-        if batch_norm:
-            bn = bn_fn(n_out)
-            ops.append(bn)
         if activation is not None:
             ops.append(nonlinearity())
+        if batch_norm:
+            ops.append(bn_fn(n_out))
     return nn.Sequential(*ops)
+
