b1b01fc11d0c97090befa0fb020a9f6d94b235f2
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-03-27 20:19:35 -0400

    Callback to check on memory leaks

--- callbacks/debug.py
+++ callbacks/debug.py
@@ -1,3 +1,4 @@
+import torch.cuda
 import subprocess
 
 
@@ -6,3 +7,12 @@ def nvidia_smi(experiment):
         print(subprocess.check_output(["nvidia-smi"]).decode(), flush=True)
 
     return nvidia_smi_callback
+
+
+def CUDAMemory(experiment):
+    def CUDAMemoryCallback(epoch):
+        max_allocated = torch.cuda.max_memory_allocated() / 1024 ** 2
+        allocated = torch.cuda.memory_allocated() / 1024 ** 2
+        print(f"Allocated CUDA memory {allocated:.0f} / {max_allocated:.0f} MB")
+
+    return CUDAMemoryCallback
