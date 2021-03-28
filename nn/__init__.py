420c37fbd1ba50061ff571954c1ea1e4d7dfc33c
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-03-27 20:11:22 -0400

    Helper HookedModule contextmanager

--- nn/__init__.py
+++ nn/__init__.py
@@ -1,2 +1,4 @@
 from .flatten import Flatten, flatten_tensors, unflatten_tensors
+from .view import View
 from .vae import VAE
+from .hooks import HookedModule
---
+++ nn/hooks.py
@@ -0,0 +1,27 @@
+from contextlib import contextmanager
+import fnmatch
+
+from torch import nn
+
+
+@contextmanager
+def HookedModule(
+    module: nn.Module, hookfn, mode: str = "forward", module_types=None, glob=None
+):
+    assert mode in ("forward", "backward")
+
+    hooks = []
+
+    for name, mod in module.named_modules():
+        if module_types and not isinstance(mod, module_types):
+            continue
+        if glob and not fnmatch.fnmatch(name, glob):
+            continue
+        if mode == "forward":
+            hooks.append(mod.register_forward_hook(hookfn))
+        else:
+            hooks.append(mod.register_backward_hook(hookfn))
+    yield module
+
+    for hook in hooks:
+        hook.remove()
