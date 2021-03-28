b37dee1ad54476ff0e12f808eb558a3a23fc5441
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-03-27 20:14:16 -0400

    Add PIL pad & trim helpers

--- util/img.py
+++ util/img.py
@@ -1,12 +1,22 @@
 import numpy as np
 from PIL import Image
+from PIL import ImageChops
+from PIL import ImageOps
+import torch
 
 
 def RGB_project(x):
     C, H, W = x.shape
-    if C % 3 == 0:
-        x = x.reshape((3, -1, H, W))
-        return x.sum(axis=1)
+    # if C % 3 == 0:
+    #     x = x.reshape((3, -1, H, W))
+    #     return x.sum(axis=1)
+    if C == 3:
+        return x
+    if C == 2:
+        y = torch.zeros(3, H, W).to(x.device)
+        y[0] = x[0]
+        y[1] = x[1]
+        return y
     else:
         return x.sum(axis=0, keepdim=True).repeat(3, 1, 1)
 
@@ -26,3 +36,23 @@ def toImg(im, norm=False):
     if norm:
         im = renorm(im)
     return Image.fromarray(im.astype(np.uint8))
+
+
+def trim(im):
+    if isinstance(im, np.ndarray):
+        im = Image.fromarray(im)
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
+
+def pad(old_im, new_size):
+    old_size = old_im.size
+    dw = new_size[0] - old_size[0]
+    dh = new_size[1] - old_size[1]
+    padding = (dw // 2, dh // 2, dw - dw // 2, dh - dh // 2)
+    new_im = ImageOps.expand(old_im, padding)
+    return new_im
