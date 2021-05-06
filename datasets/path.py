ba9a111129825d29bddbcd466b22864d25530548
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-05-05 20:06:34 -0400

    Generic dataset path solver and associated mixin

---
+++ datasets/path.py
@@ -0,0 +1,47 @@
+import os
+import pathlib
+
+
+def dataset_path(dataset, path=None):
+    """Get the path to a specified dataset
+
+    Arguments:
+        dataset {str} -- Name of the dataset
+
+    Keyword Arguments:
+        path {str} -- Semicolon separated list of paths to look for dataset folders (default: {None})
+
+    Returns:
+        dataset_path -- pathlib.Path for the first match
+
+    Raises:
+        ValueError -- If no path is provided and DATAPATH is not set
+        LookupError -- If the given dataset cannot be found
+    """
+    if path is None:
+        # Look for the dataset in known paths
+        if "DATAPATH" in os.environ:
+            path = os.environ["DATAPATH"]
+            paths = [pathlib.Path(p) for p in path.split(":")]
+        else:
+            raise ValueError(
+                f"No path specified. A path must be provided, \n \
+                           or the folder must be listed in your DATAPATH"
+            )
+
+    paths = [pathlib.Path(p) for p in path.split(":")]
+
+    for p in paths:
+        p = (p / dataset).resolve()
+        if p.exists():
+            # print(f"Found {dataset} under {p}")
+            return p
+    raise LookupError(f"Could not find {dataset} in {paths}")
+
+
+class DatapathMixin:
+
+    @property
+    def path(self):
+        return dataset_path(self.__class__.__name__)
+
