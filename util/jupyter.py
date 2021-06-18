1116634cf16bb55695e409c119276330279440a0
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-06-18 18:07:23 -0400

    Jupyter UI utils

--- util/jupyter.py
+++ util/jupyter.py
@@ -1,6 +1,6 @@
 from IPython.display import HTML, display
 
-from .functools import static_vars
+from .more_functools import static_vars
 
 @static_vars(initialized=False)
 def notebook_put_into_clipboard(text):
@@ -20,3 +20,43 @@ def notebook_put_into_clipboard(text):
         display(init)
 
     display(HTML(f'<script> updateClipboard("{text}")  </script>'))
+
+
+def disable_widget_scroll():
+    style = """
+    <style>
+       .jupyter-widgets-output-area .output_scroll {
+            height: unset !important;
+            border-radius: unset !important;
+            -webkit-box-shadow: unset !important;
+            box-shadow: unset !important;
+        }
+        .jupyter-widgets-output-area  {
+            height: auto !important;
+        }
+    </style>
+    """
+    display(HTML(style))
+
+
+def widget_dark_mode():
+    style = """
+    <style>
+        .widget-label-basic {
+            color: #ddd;
+        }
+    </style>
+    """
+    display(HTML(style))
+
+
+def jupyter_width(fraction):
+    percent = int(fraction * 100)
+    style = f"""
+    <style>
+        .container {{
+            width: {percent:d}% !important;
+        }}
+    </style>
+    """
+    display(HTML(style))
