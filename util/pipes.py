21efec12917384ab3154e13e01e63d1d5383faae
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-06-18 18:06:44 -0400

    More stdout/stderr utils

--- util/pipes.py
+++ util/pipes.py
@@ -3,12 +3,12 @@ import os
 import pathlib
 import sys
 
-@contextmanager
-def redirect_std(outfile, mode="a"):
-    with open(outfile, mode) as fo:
-        with redirect_stdout(fo):
-            yield
-    return
+# @contextmanager
+# def redirect_std(outfile, mode="a"):
+#     with open(outfile, mode) as fo:
+#         with redirect_stdout(fo):
+#             yield
+#     return
 
 # Usage is like sys.stdout = Tee(filename, mode)
 # Can also do redirect_stdout(Tee(filename, mode)) to do it temporariliy
@@ -28,3 +28,52 @@ class Tee(object):
 
     def flush(self):
         self.file.flush()
+
+
+@contextmanager
+def redirect_std(outfile, errfile, mode="a", unbuffered=True):
+    with open(outfile, mode) as fo, open(errfile, mode) as fe:
+        if unbuffered:
+            fo = Unbuffered(fo)
+            fe = Unbuffered(fe)
+        with redirect_stdout(fo), redirect_stderr(fe):
+            yield
+    return
+
+
+@contextmanager
+def temporary_save_path(filepath):
+    """Yields a path where to save a file and moves it
+    afterward to the provided location (and replaces any
+    existing file)
+    This is useful to avoid processes monitoring the filepath
+    to break if trying to read when the file is being written.
+    Note
+    ----
+    The temporary path is the provided path appended with .save_tmp
+    """
+    filepath = pathlib.Path(filepath)
+    tmppath = filepath.with_suffix(filepath.suffix + ".save_tmp")
+    assert not tmppath.exists(), "A temporary saved file already exists."
+    yield tmppath
+    if not tmppath.exists():
+        raise FileNotFoundError("No file was saved at the temporary path.")
+    if filepath.exists():
+        os.remove(filepath)
+    os.rename(tmppath, filepath)
+
+
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def writelines(self, datas):
+        self.stream.writelines(datas)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
