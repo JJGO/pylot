aa86274b241d1eda2ac8fb804de486e66bcc3a06
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-06-18 18:14:20 -0400

    S3 refactor

--- experiment/base.py
+++ experiment/base.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod
+from contextlib import contextmanager
 import datetime
 import hashlib
 import json
+import os
 import pathlib
 import random
 import shutil
@@ -13,15 +15,12 @@ import yaml
 
 import pandas as pd
 
-from ..util import MeterCSVLogger, printc
-from ..util import dict_recursive_update, expand_dots, allbut, get_full_env_info
-from ..util import TensorStore
+from ..util import dict_recursive_update, expand_dots, allbut, get_full_env_info, make_path, S3Path, printc
 from .util import fix_seed
 
 
 class Experiment:
 
-    DEFAULT_CFG = pathlib.Path("default.yml")
     NODEFAULT = object()
 
     def __init__(self, cfg=None, path=None, **kwargs):
@@ -40,21 +39,11 @@ class Experiment:
         # signal.signal(signal.SIGINT, self.SIGINT_handler)
         signal.signal(signal.SIGQUIT, self.SIGQUIT_handler)
 
-        self.csvlogger = None
-
     def _init_new(self, cfg, uid=None, **kwargs):
 
         default = {"experiment": {"seed": 42, "type": f"{self.__class__.__name__}"}}
-        # 1. Default config
-        if Experiment.DEFAULT_CFG.exists():
-            with open(Experiment.DEFAULT_CFG, "r") as f:
-                default = yaml.load(f, Loader=yaml.FullLoader)
         # 2. cfg dict or file
         if cfg is not None:
-            # File
-            if isinstance(cfg, (str, pathlib.Path)):
-                with open(cfg, "r") as f:
-                    cfg = yaml.load(f, Loader=yaml.FullLoader)
             cfg = dict_recursive_update(default, cfg)
         else:
             cfg = default
@@ -62,33 +51,34 @@ class Experiment:
         kwargs = expand_dots(kwargs)
         self.cfg = dict_recursive_update(cfg, kwargs)
 
-        root = pathlib.Path()
-        if self.get_param("log.root", False):
-            root = pathlib.Path(self.get_param("log.root"))
-
         if uid is None:
             uid = self.generate_uid()
         self.uid = uid
-        self.path = pathlib.Path(root) / self.uid
+
+        root = self.get_param('log.root', '.')
+        root = make_path(root)
+        self.path = root / self.uid
 
     def _init_existing(self, path):
         existing_cfg = pathlib.Path(path) / "config.yml"
         assert existing_cfg.exists(), "Cannot find config.yml under the provided path"
         self.path = pathlib.Path(path)
-        with open(self.path / "config.yml", "r") as f:
-            self.cfg = yaml.load(f, Loader=yaml.FullLoader)
+        with (self.path / 'config.yml').open('r') as f:
+            self.cfg = yaml.safe_load(f)
         self.uid = self.path.stem
 
     def save_config(self):
         self.path.mkdir(exist_ok=True, parents=True)
         path = self.path / "config.yml"
-        with open(path, "w") as f:
-            yaml.dump(self.cfg, f, indent=2)
+        with path.open('w') as f:
+            yaml.safe_dump(self.cfg, f, indent=2)
 
     @property
     def digest(self):
-        cfg = allbut(self.cfg, ("log",))
-        return hashlib.md5(yaml.dump(cfg, sort_keys=True).encode("utf-8")).hexdigest()
+        cfg = allbut(self.cfg, ("log", "tags"))
+        return hashlib.md5(
+            yaml.safe_dump(cfg, sort_keys=True).encode("utf-8")
+        ).hexdigest()
 
     def __hash__(self):
         return int(self.digest, 16)
@@ -117,23 +107,17 @@ class Experiment:
         # printc(f"Logging results to {self.path}", color="MAGENTA")
         print(f"{self.path}")
 
-        self.csvlogger = MeterCSVLogger(self.logs_path)
+        # self.csvlogger = MeterCSVLogger(self.logs_path)
+        # self.tensor_store = TensorStore(self.path / "store.hdf5")
 
         # Save environment for repro
         envinfo_path = self.path / "env.yml"
-        with open(envinfo_path, "a+") as f:
-            yaml.dump([get_full_env_info()], f)
-
-        self.tensor_store = TensorStore(self.path / 'store.hdf5')
+        if not envinfo_path.exists():
+            envinfo_path.touch()
+        with envinfo_path.open('a') as f:
+            yaml.safe_dump([get_full_env_info()], f)
 
         self.save_config()
-
-    def log(self, *args, **kwargs):
-        self.csvlogger.set(*args, **kwargs)
-
-    def dump_logs(self):
-        self.csvlogger.set(timestamp=time.time())
-        self.csvlogger.flush()
 
     # def SIGINT_handler(self, signal, frame):
     #     pass
@@ -143,13 +127,16 @@ class Experiment:
         sys.exit(1)
 
     def delete(self):
-        shutil.rmtree(self.path, ignore_errors=True)
+        if isinstance(self.path, S3Path):
+            self.path.rmtree()
+        else:
+            shutil.rmtree(self.path, ignore_errors=True)
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.cfg})"
 
     def __str__(self):
-        return f"{self.__class__.__name__}\n---\n" + yaml.dump(self.cfg, indent=2)
+        return f"{self.__class__.__name__}\n---\n" + yaml.safe_dump(self.cfg, indent=2)
 
     @abstractmethod
     def run(self):
@@ -181,31 +168,41 @@ class Experiment:
     def save_metrics(self, **metrics):
 
         # Save as JSONL file
-        with open(self.metrics_path, "a") as f:
+        with self.metrics_path.open('a') as f:
             print(json.dumps(metrics), file=f)
 
     def batch_save_metrics(self, metrics):
 
-        with open(self.metrics_path, "a") as f:
+        with self.metrics_path.open("a") as f:
             for metric in metrics:
                 print(json.dumps(metric), file=f)
 
     @property
     def metrics(self):
         if self.metrics_path.exists():
-            with open(self.metrics_path, "r") as f:
+            with self.metrics_path.open('r') as f:
                 return [json.loads(line) for line in f.readlines()]
 
     @property
     def metrics_df(self):
         if self.metrics_path.exists():
-            return pd.read_json(self.metrics_path, lines=True)
+            with self.metrics_path.open('r') as f:
+                return pd.read_json(f, lines=True)
 
-    @property
-    def logs_path(self):
-        return self.path / 'logs.csv'
+    # LOGS API IS DEPRECATED IN FAVOR OF THE METRICS API
 
-    @property
-    def logs_df(self):
-        if self.logs_path.exists():
-            return pd.read_csv(self.logs_path)
+    #     def log(self, *args, **kwargs):
+    #         self.csvlogger.set(*args, **kwargs)
+
+    #     def dump_logs(self):
+    #         self.csvlogger.set(timestamp=time.time())
+    #         self.csvlogger.flush()
+
+    #     @property
+    #     def logs_path(self):
+    #         return self.path / "logs.csv"
+
+    # @property
+    # def logs_df(self):
+    #     if self.logs_path.exists():
+    #         return pd.read_csv(self.logs_path)
