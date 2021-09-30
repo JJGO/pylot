6446fbca8168e2bae04c89d9475acc7ce88eab9b
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-09-30 11:25:08 -0400

    Reimplement TotalVariationLoss

--- loss/total_variation.py
+++ loss/total_variation.py
@@ -1,11 +1,47 @@
+from typing import Optional
+
 import torch
+from torch import Tensor
 
-def total_variation2d(x):
-    batch_size, C, H, W = x.size()
+from .util import _loss_module_from_func
 
-    dh = torch.abs(x[..., :-1, :-1] - x[..., 1:, :-1])
-    dw = torch.abs(x[..., :-1, :-1] - x[..., :-1, 1:])
-    dh = dh.view(dh.size(0), -1)
-    dw = dw.view(dw.size(0), -1)
-    tv = torch.sum(dh, axis=-1) + torch.sum(dw, axis=-1)
-    return tv
+
+def total_variation_loss(
+    y_pred: Tensor,
+    y_true: Optional[Tensor] = None,
+    penalty: str = "l1",
+    reduction: str = "mean",
+):
+
+    assert reduction in ("sum", "mean", None)
+    assert penalty in (
+        "l1",
+        "l2",
+    )
+    shape = y_pred.shape[2:]
+    ndims = len(shape)
+
+    s_all = slice(None, None, None)  # [:]
+    s_front = slice(None, -1, None)  # [:-1]
+    s_back = slice(1, None, None)  # [1:]
+
+    dxs = [
+        y_pred[[s_back if i == j else s_all for j in range(-2, ndims)]]
+        - y_pred[[s_front if i == j else s_all for j in range(-2, ndims)]]
+        for i in range(ndims)
+    ]
+
+    if penalty == "l1":
+        dxs = [torch.abs(dx) for dx in dxs]
+    elif penalty == "l2":
+        dxs = [torch.square(dx) for dx in dxs]
+
+    if reduction is None:
+        return dxs
+    if reduction == "sum":
+        return sum(dx.sum() for dx in dxs)
+    if reduction == "mean":
+        return sum(dx.mean() for dx in dxs) / ndims
+
+
+TotalVariationLoss = _loss_module_from_func("TotalVariationLoss", total_variation_loss)
