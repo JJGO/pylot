731a437cdbcc13707018e585388ea0750ca497a9
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-09-30 11:50:24 -0400

    Fix imports

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -3,7 +3,13 @@
 # TODO ReduceLR on plateau?
 
 from .checkpoint import Checkpoint
-from .log import LogParameters, TqdmParameters, PrintLogged, ETA
-from .setup import Summary, Topology, ParameterTable, ModuleTable
+from .log import LogParameters, TqdmParameters, PrintLogged, ETA, JobProgress
+from .setup import (
+    Summary,
+    Topology,
+    ParameterTable,
+    ModuleTable,
+    CheckHalfCosineSchedule,
+)
 from .debug import nvidia_smi, GPUStats
-from .img import ImgIO, ImgActivations 
+from .img import ImgIO, ImgActivations
