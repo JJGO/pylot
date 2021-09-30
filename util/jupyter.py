43b79dc62ba5d077fd2906bfe187bb64fbcc02f8
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-09-30 11:37:30 -0400

    Determine whether we are on a jupyter notebook

--- util/jupyter.py
+++ util/jupyter.py
@@ -2,6 +2,20 @@ from IPython.display import HTML, display
 
 from .more_functools import static_vars
 
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        elif shell == "TerminalInteractiveShell":
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
 @static_vars(initialized=False)
 def notebook_put_into_clipboard(text):
 
