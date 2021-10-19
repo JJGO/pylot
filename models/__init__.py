b190c344dbf1d9f13f63e359060ea7c222a1cdab
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-10-18 23:59:00 -0400

    Add voxelmorph implementation

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .vision import *
 from .unet import UNet
 from .vae import VAE
+from .voxelmorph import Voxelmorph
---
+++ models/voxelmorph.py
@@ -0,0 +1,31 @@
+from typing import Dict, Tuple, Any
+
+import torch
+from torch import nn, Tensor
+
+from pylot.nn.spatial_transformer import SpatialTransformer
+from pylot.models import UNet
+
+
+class Voxelmorph(nn.Module):
+    def __init__(
+        self,
+        size: Tuple[int, ...],
+        unet: Dict[str, Any],
+        in_channels=2,
+    ):
+        super().__init__()
+        self.in_channels = in_channels
+        dims = len(size)
+        self.registration_unet = UNet(
+            in_channels=in_channels, out_channels=dims, **unet
+        )
+        self.spatial_transform = SpatialTransformer(size)
+
+    def forward(self, Xm: Tensor, Xf: Tensor) -> Dict[str, Tensor]:
+
+        phi = self.registration_unet(torch.cat([Xm, Xf], dim=1))
+
+        Xm_phi = self.spatial_transform(Xm, phi)
+
+        return dict(Xm_phi=Xm_phi, phi=phi)
