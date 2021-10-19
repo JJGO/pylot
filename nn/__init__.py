abf9cb1d89d944ad1383df2ccf2d6b654227014c
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-10-18 23:23:40 -0400

    Reorder imports to prevent circular import issue

--- nn/__init__.py
+++ nn/__init__.py
@@ -1,3 +1,5 @@
+from .nonlinearity import get_nonlinearity, register_nonlinearity
+from .init import initialize_layer, initialize_weight, initialize_bias
 from .flatten import Flatten, flatten_tensors, unflatten_tensors
 from .view import View
 from .vae import VAE
@@ -5,5 +7,3 @@ from .hooks import HookedModule
 from .util import num_params
 from .conv import *
 from .interpolation import resize
-from .init import initialize_layer, initialize_tensor
-from .nonlinearity import get_nonlinearity, register_nonlinearity
