64428380c82b03a0c4780a85f05761a669367932
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-12-14 19:45:24 -0500

    Expand hooks functionality

--- nn/hooks.py
+++ nn/hooks.py
@@ -6,13 +6,18 @@ from torch import nn
 
 @contextmanager
 def HookedModule(
-    module: nn.Module, hookfn, mode: str = "forward", module_types=None, glob=None
+    module: nn.Module,
+    hookfn,
+    mode: str = "forward",
+    module_types=None,
+    glob=None,
+    recurse=True,
 ):
     assert mode in ("forward", "backward")
 
     hooks = []
 
-    for name, mod in module.named_modules():
+    for name, mod in module.named_modules() if recurse else [("", module)]:
         if module_types and not isinstance(mod, module_types):
             continue
         if glob and not fnmatch.fnmatch(name, glob):
@@ -20,10 +25,10 @@ def HookedModule(
         if mode == "forward":
             hooks.append(mod.register_forward_hook(hookfn))
         else:
-            hooks.append(mod.register_backward_hook(hookfn))
+            # hooks.append(mod.register_backward_hook(hookfn))
+            hooks.append(mod.register_full_backward_hook(hookfn))
     try:
         yield module
     finally:
         for hook in hooks:
             hook.remove()
-
