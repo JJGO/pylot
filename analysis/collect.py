5633e551c5401e98d902fe8405bb95419817049a
Jose Javier <3844846+JJGO@users.noreply.github.com>
2022-06-21 17:01:15 -0400

    Reimpl metrics collection with new setup

--- analysis/collect.py
+++ analysis/collect.py
@@ -1,13 +1,27 @@
-from collections import Counter
+import getpass
 import itertools
+import json
 import pathlib
+import collections
+from typing import Optional
+from fnmatch import fnmatch
 
+from tqdm.auto import tqdm
 import pandas as pd
-from tqdm.autonotebook import tqdm
+import numpy as np
 
-from .data import drop_std
+from ..util.config import HDict, valmap, keymap
 from ..util import FileCache
-from ..util import expand_keys, delete_with_prefix
+
+# unused import because we want the patching side-effects
+# on pd.DataFrames
+from ..pandas import api
+
+
+def list2tuple(val):
+    if isinstance(val, list):
+        return tuple(map(list2tuple, val))
+    return val
 
 
 def shorthand_names(names):
@@ -16,7 +30,7 @@ def shorthand_names(names):
         parts = c.split(".")
         for i, _ in enumerate(parts, start=1):
             cols.append("_".join(parts[-i:]))
-    counts = Counter(cols)
+    counts = collections.Counter(cols)
     column_renames = {}
     for c in names:
         parts = c.split(".")
@@ -28,127 +42,115 @@ def shorthand_names(names):
     return column_renames
 
 
-def shorthand_columns(df, return_dict=False):
+def shorthand_columns(df):
     column_renames = shorthand_names(df.columns)
     df.rename(columns=column_renames, inplace=True)
-    if return_dict:
-        return df, column_renames
     return df
-
-
-def add_experiment_metadata(df):
-
-    path = df["experiment.path"]
-    df["experiment.hash"] = path.map(lambda p: p.name.split("-")[3])
-    df["experiment.nonce"] = path.map(lambda p: p.name.split("-")[2])
-    df["experiment.create_time"] = path.map(lambda p: p.name[: len("YYYYMMDD-hhmmss")])
-    return df
-
-
-def dedup_df(df):
-    uniq = {}
-    for c in df.columns:
-        if df[c].nunique(dropna=False) == 1:
-            uniq[c] = df[c].unique()[0]
-    df.drop(columns=list(uniq.keys()), inplace=True)
-    return uniq
-
-
-def list2tuple(val):
-    if isinstance(val, list):
-        return tuple(map(list2tuple, val))
-    return val
 
 
 class ResultsLoader:
-    def __init__(self, cache_file="/tmp/pylot-results.cache"):
-        self.filecache = FileCache(cache_file)
-        self.load_metrics = self.load_logs
+    def __init__(self, cache_file: Optional[str] = None, num_workers: int = 8):
+        unixname = getpass.getuser()
+        cache_file = cache_file or f"/tmp/{unixname}-results.diskcache"
+        self._cache = FileCache(cache_file)
+        self._num_workers = num_workers
 
     def load_configs(
-        self, *paths, shorthand=True, dedup=False, metadata=False, categories=False
-    ):
-        columns = {}
-
-        # folders = pathlib.Path(path).iterdir
-        folders = itertools.chain.from_iterable(
-            pathlib.Path(path).iterdir() for path in paths
-        )
-        total = sum(1 for _ in folders)
-        folders = itertools.chain.from_iterable(
-            pathlib.Path(path).iterdir() for path in paths
-        )
-        paths = []
-
-        i = 0
-        for folder in tqdm(folders, total=total, leave=False):
-            cfg = self.filecache.get(folder / "config.yml")
-            if cfg is None:
-                continue
-
-            # cfg = delete_with_prefix(expand_keys(cfg), "log")
-            cfg.pop("log", None)
-            cfg = expand_keys(cfg)
-
-            for c in columns:
-                val = cfg.get(c, None)
-                columns[c].append(list2tuple(val))
-            for c in cfg:
-                if c not in columns:
-                    columns[c] = [None] * i
-                    columns[c].append(list2tuple(cfg[c]))
-
-            paths.append(folder)
-            i += 1
-
-        columns["experiment.path"] = paths
-        df = pd.DataFrame.from_dict(columns)
-
-        if metadata:
-            add_experiment_metadata(df)
-        self.filecache.dump()
-        if shorthand:
-            df = shorthand_columns(df)
-
-        if dedup:
-            df.attrs["uniq"] = dedup_df(df)
-        if categories:
-            df = df.to_categories()
-        return df
-
-    def load_logs(
         self,
         *paths,
         shorthand=True,
-        dedup=False,
+        properties=False,
         metadata=False,
-        df=None,
-        file="metrics",
+        log=False,
+        callbacks=False,
     ):
-        if len(paths) > 0:
-            df = self.load_configs(
-                *paths, shorthand=True, dedup=dedup, metadata=metadata
+
+        folders = list(
+            itertools.chain.from_iterable(
+                pathlib.Path(path).iterdir() for path in paths
             )
+        )
+
+        configs = self._cache.gets(
+            (folder / "config.yml" for folder in folders), num_workers=self._num_workers
+        )
+
+        rows = []
+        for folder, cfg in tqdm(zip(folders, configs), leave=False, total=len(folders)):
+            if cfg is None:
+                continue
+            cfg = HDict(cfg)
+            if not log:
+                cfg.pop("log", None)
+            if not callbacks:
+                cfg.pop("callbacks", None)
+            if metadata:
+                cfg.update({"metadata": self._cache[folder / "metadata.json"]})
+            if properties:
+                cfg.update({"properties": self._cache[folder / "properties.json"]})
+
+            flat_cfg = valmap(list2tuple, cfg.flatten())
+            flat_cfg["path"] = folder
+
+            flat_cfg = keymap(lambda x: x.replace("._class", ""), flat_cfg)
+            flat_cfg = keymap(lambda x: x.replace("._fn", ""), flat_cfg)
+
+            rows.append(flat_cfg)
+
+        df = pd.DataFrame.from_records(rows)
+
+        for col in df.columns:
+            if not df[col].map(pd.api.types.is_hashable).all():
+                df[col] = df[col].map(json.dumps)
+
+        if shorthand:
+            df = shorthand_columns(df)
+        return df
+
+    def load_metrics(
+        self,
+        config_df,
+        file="metrics.jsonl",
+        prefix="log",
+        shorthand=True,
+        copy_cols=None,
+        path_key=None,
+    ):
 
         log_dfs = []
+        if path_key is None:
+            path_key = "path"
+        if copy_cols is None:
+            copy_cols = config_df.columns.to_list()
 
-        for _, row in tqdm(df.iterrows(), total=len(df), leave=False):
-            path = row["path"] if shorthand else row["experiment.path"]
-            log_df = self.filecache.get(path / f"{file}.jsonl")
+        folders = config_df[path_key].values
+
+        files = self._cache.gets(
+            (folder / file for folder in folders), num_workers=self._num_workers
+        )
+
+        for (_, row), log_df in tqdm(
+            zip(config_df.iterrows(), files), total=len(config_df), leave=False
+        ):
+            row = row.to_dict()
+            path = pathlib.Path(row[path_key])
             if log_df is None:
                 continue
-
-            log_df.rename(columns={c: f"log.{c}" for c in log_df.columns}, inplace=True)
+            if prefix:
+                log_df.rename(
+                    columns={c: f"{prefix}.{c}" for c in log_df.columns}, inplace=True
+                )
+            if len(copy_cols) > 0:
+                for col in copy_cols:
+                    val = row[col]
+                    if isinstance(val, tuple):
+                        log_df[col] = np.array(itertools.repeat(val, len(log_df)))
+                    else:
+                        log_df[col] = val
             log_df["path"] = path
-
-            # for k, v in row.items():
-            #     log_df[k] = [v for _ in range(len(log_df))]
             log_dfs.append(log_df)
 
-        self.filecache.dump()
-
         full_df = pd.concat(log_dfs, ignore_index=True)
-        full_df = pd.merge(df, full_df, on="path")
 
         if shorthand:
             renames = {}
@@ -161,115 +163,43 @@ class ResultsLoader:
                         renames[c] = c.replace(".", "__")
             full_df.rename(columns=renames, inplace=True)
 
-        full_df.attrs["exp_cols"] = list(df.columns)
-        full_df.attrs["log_cols"] = [c for c in full_df.columns if c not in df.columns]
-        if dedup:
-            full_df.attrs["uniq"] = df.attrs["uniq"]
-
         return full_df
 
-    def load_parquets(
-        self, file, df, prefix=None, shorthand=True, skip=False, copy_cols=("path",),
+    def load_aggregate(
+        self, config_df, metric_df, agg=None, metrics_groupby=("phase",)
     ):
 
-        dfxs = []
+        config_cols = list(config_df.columns)
+        metric_cols = list(set(metric_df.columns) - set(config_df.columns))
 
-        for _, row in tqdm(df.iterrows(), total=len(df), leave=False):
-            path = row["path"] if shorthand else row["experiment.path"]
-            if skip and not (path / f"{file}.parquet").exists():
-                continue
-            dfx = pd.read_parquet(path / f"{file}.parquet")
+        _agg_fns = collections.defaultdict(list)
 
-            if prefix:
-                dfx.rename(
-                    columns={c: f"{prefix}.{c}" for c in dfx.columns}, inplace=True
-                )
+        DEFAULT_AGGS = {
+            "max": ["*acc*", "*score*", "*epoch*"],
+            "min": ["*loss*", "*err*"],
+        }
 
-            if copy_cols is not None:
-                for c in copy_cols:
-                    v = row[c]
-                    if v is None or isinstance(v, (str, float, int, pathlib.Path)):
-                        dfx[c] = v
-                    else:
-                        dfx[c] = [v for _ in range(len(dfx))]
-
-            # for k, v in row.items():
-            #     if v is None or isinstance(v, (str, float, int)):
-            #         dfx[k] = v
-            # dfx[k] = [v for _ in range(len(dfx))]
-
-            # Merge is slow
-            # dfx = pd.merge(
-            #     row.to_frame().transpose(), dfx, on="path", suffixes=("", "_data")
-            # )
-            dfxs.append(dfx)
-
-        full_df = pd.concat(dfxs, ignore_index=True)
-        # full_df = pd.merge(df, full_df, on='path', suffixes=('', "_data"))
-
-        return full_df
-
-    def load_agg_logs(
-        self, *paths, agg=None, shorthand=True, dedup=False, metadata=False, df=None
-    ):
-        if len(paths) > 0:
-            df = self.load_logs(
-                *paths, shorthand=shorthand, dedup=dedup, metadata=metadata
-            )
-
-        _agg_fns = {}
-        for c in df.attrs["log_cols"]:
-            if c.startswith("t_"):  # Timing, mean
-                _agg_fns[c] = "mean"
-            elif "loss" in c:  # Min loss
-                _agg_fns[c] = ["min"]
-            elif "acc" in c:  # Accuracy, max
-                _agg_fns[c] = ["max"]
-            elif "err" in c:  # Error, min
-                _agg_fns[c] = ["min"]
-            elif "epoch" in c:
-                _agg_fns[c] = "max"
-            elif "score" in c:
-                _agg_fns[c] = ["max"]
-
+        for agg_fn, patterns in DEFAULT_AGGS.items():
+            for column in metric_cols:
+                if any(fnmatch(column, p) for p in patterns):
+                    _agg_fns[column].append(agg_fn)
         if agg is not None:
-            _agg_fns.update(agg)
+            for column, agg_fns in agg.items():
+                _agg_fns[column].extend(agg)
 
-        # g = "path" if shorthand else "experiment.path"
-        g = df.attrs["exp_cols"] + ["phase"] if "phase" in df.attrs["log_cols"] else []
-        agg_df = df.groupby(g, as_index=False, dropna=False, observed=True).agg(
+        g = config_cols + list(metrics_groupby)
+        agg_df = metric_df.groupby(g, as_index=False, dropna=False, observed=True).agg(
             _agg_fns
         )
         agg_df.columns = [
             col if agg == "" else f"{agg}_{col}" for col, agg in agg_df.columns.values
         ]
 
-        # agg_df = pd.merge(agg_df, exp_df, how="left", on=g)
-
-        agg_df.attrs = df.attrs
-        agg_df.attrs["log_cols"] = [c for c in agg_df.columns if c not in g]
         return agg_df
 
-    def load_all(
-        self,
-        *paths,
-        shorthand=True,
-        dedup=False,
-        metadata=False,
-        categories=False,
-        **selector,
-    ):
-        dfc = self.load_configs(
-            *paths,
-            shorthand=shorthand,
-            dedup=dedup,
-            metadata=metadata,
-            categories=categories,
-        )
-        from .. import pandas
+    def load_all(self, *paths, shorthand=True, **selector):
 
-        dfc = dfc.select(**selector)
-        df = self.load_logs(df=dfc)
-        dfa = self.load_agg_logs(df=df)
-
+        dfc = self.load_configs(*paths, shorthand=shorthand,).select(**selector)
+        df = self.load_metrics(dfc)
+        dfa = self.load_aggregate(dfc, df)
         return dfc, df, dfa
