64402151603ac62f41504f9931ea0fe54857c44d
Jose Javier <3844846+JJGO@users.noreply.github.com>
2022-11-09 09:52:11 -0500

    Add scantree

--- util/filesystem.py
+++ util/filesystem.py
@@ -1,6 +1,9 @@
+import os
+
 def get_fs_backend(path):
     import subprocess
     import io
+    import pandas as pd
     output = subprocess.check_output(['df', '-T', str(path)]).decode()
     buf = io.StringIO(output.replace('Mounted on', 'mountpoint'))
     df = pd.read_csv(buf, delim_whitespace=True)
@@ -13,3 +16,13 @@ def get_fs_backend(path):
     if fs in ('nfs', 'nfs4', 'afs', 'cifs', 'fuse.sshfs'):
         return 'remote'
     return 'unknown'
+
+
+
+def scantree(path):
+    """Recursively yield DirEntry objects for given directory."""
+    for entry in os.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scantree(entry.path)  # see below for Python 2.x
+        elif entry:
+            yield entry
