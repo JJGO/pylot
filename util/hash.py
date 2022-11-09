7fdb94fc4f9b6dd3dc15c42bf00dc083a9775254
Jose Javier <3844846+JJGO@users.noreply.github.com>
2022-11-09 09:52:45 -0500

    Convenience hash functions

--- util/hash.py
+++ util/hash.py
@@ -1,9 +1,16 @@
 import copy
 import hashlib
 import json
+from pydantic import validate_arguments
+import pathlib
+import zlib
+import xxhash
+
+DEFAULT_CHUNKSIZE = 64 * 1024
+
 
 def make_hash(o):
-    raise ValueError('hash is invalid across runtimes')
+    raise ValueError("hash is invalid across runtimes")
     """
     Makes a hash from a dictionary, list, tuple or set to any level, that contains
     only other hashable types (including any lists, tuples, sets, and
@@ -30,3 +37,37 @@ def make_hash(o):
 def json_digest(data) -> str:
     h = hashlib.sha256(json.dumps(data).encode()).hexdigest()
     return h
+
+
+@validate_arguments
+def file_crc(path: pathlib.Path, chunksize=DEFAULT_CHUNKSIZE) -> str:
+    crc = 0
+    with open(path, "rb") as f:
+        while True:
+            data = f.read(chunksize)
+            if not data:
+                break
+            crc = zlib.adler32(data, crc)
+    crc &= 0xFFFFFFFF
+    return f"{crc:x}"
+
+
+@validate_arguments
+def file_digest(path: pathlib.Path, chunksize=DEFAULT_CHUNKSIZE) -> str:
+    # Use xxhash as it's substantially faster than md5
+    # See https://github.com/Cyan4973/xxHash
+    h = xxhash.xxh3_64()
+    with open(path, "rb") as f:
+        while True:
+            data = f.read(chunksize)
+            if not data:
+                break
+            h.update(data)
+    return h.hexdigest()
+
+
+@validate_arguments
+def fast_file_digest(path: pathlib.Path) -> str:
+    import imohash
+
+    return imohash.hashfile(path, hexdigest=True)
