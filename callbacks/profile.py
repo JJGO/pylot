9767b8f072db4d649da21b5a8db1e5dc691ac834
Jose Javier <3844846+JJGO@users.noreply.github.com>
2022-11-09 09:51:05 -0500

    Discard early iterations of profiling

--- callbacks/profile.py
+++ callbacks/profile.py
@@ -8,33 +8,34 @@ from ..util.timer import StatsCUDATimer, StatsTimer
 from ..util.torchutils import to_device
 
 
-def Throughput(experiment, n_iter=100):
+def Throughput(experiment, n_iter: int = 1_00, verbose: bool = True):
 
-    if 'cuda' in str(experiment.device):
-        timer = StatsCUDATimer(unit="ms")
+    if "cuda" in str(experiment.device):
+        timer = StatsCUDATimer(unit="ms", skip=n_iter//10)
     else:
-        timer = StatsTimer(unit='ms')
+        timer = StatsTimer(unit="ms", skip=n_iter//10)
 
     # Dataloader throughput
     dl = iter(itertools.cycle(experiment.train_dl))
-    for _ in range(n_iter):
+    for _ in range(n_iter+n_iter//10):
         with timer("train-dl"):
             _ = next(dl)
 
     # Model training throughput
     sample_input = to_device(next(iter(experiment.train_dl)), experiment.device)
-    for _ in range(n_iter):
+    for _ in range(n_iter+n_iter//10):
         with timer("train-loop"):
-            experiment.run_step(batch=sample_input, batch_idx=0, backward=True)
+            experiment.run_step(batch=sample_input, batch_idx=0, backward=True, augmentation=True)
 
     timer_df = timer.measurements_df()
     timer_df = timer_df.set_index("label")
 
-    print(tabulate(timer_df, headers="keys"), flush=True)
+    if verbose:
+        print(tabulate(timer_df, headers="keys"), flush=True)
 
     t_dl = timer["train-dl"].mean
     t_gpu = timer["train-loop"].mean
-    if t_dl > t_gpu:
+    if t_dl > t_gpu and verbose:
         printc(
             f"Experiment is dataloader bound dl={t_dl:.2f}ms > gpu={t_gpu:.2f}ms",
             color="RED",
@@ -43,3 +44,5 @@ def Throughput(experiment, n_iter=100):
             experiment.train_dl.num_workers * t_dl / t_gpu
         )
         printc(f"Try setting num_workers={recommended_num_workers}", color="RED")
+
+    return timer_df
